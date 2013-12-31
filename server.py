66d418d8adc54aa90ce08618d514f5a87f9db437
camerazn <christopheragocs@cagocs.backstop.local>
2013-12-31 09:52:37 -0600

    Refactored to use Flask instead of Bottle. \nBroke the gif generation code out into it's own method.

--- server.py
+++ server.py
@@ -1,17 +1,23 @@
-from bottle import run, route, response
+#from bottle import run, route, response
+from flask import Flask, stream_with_context, request, Response
 from PIL import Image
 import StringIO
 
-@route('/')
+
+app = Flask(__name__)
+
+@app.route('/stream')
 def index():
-	response.content_type = "image/gif"
+	#response.content_type = "image/gif"
+	return Response(stream_with_context(generate()), mimetype='image/gif')
+
+
+def generate():
 	img = Image.new("RGB", (1000,2000), "#666666")
 	retString = StringIO.StringIO()
 	img.save(retString, "GIF")
-
-	return retString.getvalue()
-
+	yield retString.getvalue()
 
 
 if __name__ == "__main__":
-	run(host="localhost", port=4000)
+	app.run(host="localhost", port=4000)
